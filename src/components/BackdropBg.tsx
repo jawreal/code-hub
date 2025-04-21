f40c2637f41285b9611672ded62423427e9035bf
Jorell Relleve <jorellrelleve@gmail.com>
2025-04-21 20:46:06 +0800

    remove blur effect & add animation in modal

--- src/components/BackdropBg.tsx
+++ src/components/BackdropBg.tsx
@@ -10,7 +10,7 @@ interface BACKDROP_TYPE {
 const BackdropBg = ({ show, setBackdrop, position , children }: BACKDROP_TYPE) => {
   
   return (
-    <div onClick={() => setBackdrop(false)} className={`fixed pt-2 flex z-50 justify-center ${position ?? "items-center"} inset-0 transition-all ${show ? 'visible bg-black/20 backdrop-blur-sm' : 'invisible'} `}>
+    <div onClick={() => setBackdrop(false)} className={`fixed pt-2 flex z-50 justify-center ${position ?? "items-center"} inset-0 transition-all ${show ? 'visible bg-black/20' : 'invisible'} `}>
     { children } 
     </div>
     );
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,7 +11,7 @@ interface MODAL_TYPE  {
 
 const Modal = ({ openModal, setModal, children, showCloseBtn }: MODAL_TYPE ) => {
   return(
-     <div onClick={(e) => e.stopPropagation()} className={`dark:bg-zinc-900 bg-zinc-50 w-full md:max-w-[31rem] mx-3 rounded-md border border-zinc-200 dark:border-zinc-800 p-3 flex flex-col justify-center items-center text-center transition-all duration-300 ease-in-out ${openModal ? 'opacity-100 visible' : 'opacity-0 invisible'} relative`}>
+     <div onClick={(e) => e.stopPropagation()} className={`dark:bg-zinc-900 bg-zinc-50 w-full md:max-w-[31rem] mx-3 rounded-md border border-zinc-200 dark:border-zinc-800 p-3 flex flex-col justify-center items-center text-center transform transition-all duration-300 ease-in-out ${openModal ? 'scale-100 opacity-100 visible' : 'scale-90 opacity-0 invisible'} relative`}>
        {showCloseBtn && <Button className="p-1 rounded-md border border-zinc-200 dark:border-zinc-800 text-zinc-400 dark:text-zinc-200 bg-zinc-200/30 dark:bg-zinc-900/40 absolute right-0" icon={<X size={22} />} onClick={() => setModal(false)} />} 
        { children } 
       </div>
